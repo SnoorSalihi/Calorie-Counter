3f5dcd702be46998c08e0f8b325cee6aea6c7664
SnoorSalihi <eng.sinoor@gmail.com>
2024-05-25 23:55:52 -0400

    update4

--- script.js
+++ script.js
@@ -19,4 +19,7 @@ function cleanInputString(str) {
     const regex = /\d+e\d+/i;
     return str.match(regex);
   }
-  console.log(isInvalidInput("1e3"));
+  //console.log(isInvalidInput("1e3"));
+  function addEntry() {
+   targetId = "#" + entryDropdown.value;
+}
